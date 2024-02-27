40bbd24e45a860f6bec0c5cd358f4778023d8c66
JeroenRoodIHS <37173130+JeroenRoodIHS@users.noreply.github.com>
2024-02-27 12:13:37 +0100

    Update getFontWeight.ts
    Adding support for more fontWeight names. Instead of "semi-bold", sometimes "semiBold" is used. This is naming convention used more often by fonts hosted by Google Fonts (Noto Sans, Montserrat and Rokkitt, for example).

--- src/utils/text/getFontWeight.ts
+++ src/utils/text/getFontWeight.ts
@@ -1,26 +1,17 @@
 export const getFontWeight = (fontWeight: string) => {
   const inputFontWeight = fontWeight.toLowerCase();
 
-  const weights = {
-    thin: 100,
-    hairline: 100,
-    "extra-light": 200,
-    "ultra-light": 200,
-    light: 300,
-    normal: 400,
-    regular: 400,
-    book: 400,
-    medium: 500,
-    "semi-bold": 600,
-    "demi-bold": 600,
-    bold: 700,
-    "extra-bold": 800,
-    "ultra-bold": 800,
-    black: 900,
-    heavy: 900,
-    "extra-black": 950,
-    "ultra-black": 950,
-  };
-
-  return weights[inputFontWeight] || 400;
+  const weightMapping = {
+    100 : ["thin", "hairline", "100"],
+    200 : ["extra-light", "extraLight", "ultra-light", "ultraLight", "200"],
+    300 : ["light", "300"],
+    400 : ["normal", "regular", "book", "400"],
+    500 : ["medium", "500"],
+    600 : ["semi-bold", "semiBold", "demi-bold", "demiBold", "600"],
+    700 : ["bold", "700"],
+    800 : ["ultra-bold", "ultraBold", "extra-bold", "extraBold", "800"],
+    900 : ["black", "heavy", "900"],
+    950 : ["extra-black", "ultra-black", "extraBlack", "ultraBlack", "950"]
+  }
+  return Object.keys(weightMapping).find(weight => weightMapping[weight].includes(inputFontWeight)) || 400;
 };
